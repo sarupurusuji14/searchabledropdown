90731780a36761e7858d75309086bfc539b2436c
sarupurusuji14 <60377106+sarupurusuji14@users.noreply.github.com>
2020-07-22 16:58:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// Angular
+
+var phonecatApp = angular.module('app', []);
+
+phonecatApp.controller('ListCtrl', function ($scope) {
+    $scope.items = [{
+        'name': 'Item 1'
+    }, {
+        'name': 'Item 2'
+    }, {
+        'name': 'Account 3'
+    }, {
+        'name': 'Account 4'
+    }, {
+        'name': 'Item 5'
+    }, {
+        'name': 'Item 6'
+    }, {
+        'name': 'User 7'
+    }, {
+        'name': 'User 8'
+    }, {
+        'name': 'Item 9'
+    }, {
+        'name': 'Item 10'
+    }, {
+        'name': 'Item 11'
+    }, {
+        'name': 'Item 12'
+    }, {
+        'name': 'Item 13'
+    }, {
+        'name': 'Item 14'
+    }, {
+        'name': 'User 15'
+    }, {
+        'name': 'User 16'
+    }, {
+        'name': 'Person 17'
+    }, {
+        'name': 'Person 18'
+    }, {
+        'name': 'Person 19'
+    }, {
+        'name': 'Item 20'
+    }, ];
+});
